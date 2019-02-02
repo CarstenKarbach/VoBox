a83036f428ab12912beddcd5bf639d6ac7a326e6
CarstenKarbach <c.karbach.apps@gmail.com>
2019-02-02 14:05:34 +0100

    Upgrade VoBox to new Android Studio version 3.3

--- app/src/main/java/de/karbach/superapp/SingleFragmentActivity.java
+++ app/src/main/java/de/karbach/superapp/SingleFragmentActivity.java
@@ -24,7 +24,6 @@ import android.app.FragmentManager;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.NavUtils;
-import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuInflater;
